1d99e92b7b8f143477cd7956c36ffa0bb63d1590
miguelquesadamartinez <miguel.quesada.martinez.1975@gmail.com>
2026-02-06 14:39:06 +0100

    Seems bug

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -63,7 +63,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        $roles[] = 'ROLE_USER';
+        //$roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
